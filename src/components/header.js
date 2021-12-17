cb418f754dae5a2210ab44983e3996855e091fa1
Kumari <904657@cognizant.com>
2021-12-17 16:34:59 +0530

    created

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/header.css';
+
+const Header = () =>{
+    return(
+        <div id="header">
+            <a>MyWeb</a>
+            <a href='/'>Home</a>
+            <a id="float">
+            
+            Hey, Welcome </a>
+        </div>
+    )
+};
+
+export default Header;
