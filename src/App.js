96667bdb194a51f8f9ce2c17e1e3a9d8e8715c33
Rahul-1999 <rh.rahul.9480@gmail.com>
2021-12-18 16:39:38 +0530

    Sidbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
-import Footer from './components/footer';
-import Header from './components/header';
+import Footer from './components/Footer';
+import Main from "./components/Main";
+//import Header from './components/Header';
+import Sidebar from './components/SideBar';
 
 function App() {
   return (
     <div className="app"> 
-      
-      <Header/>
+     {/* <Header/> */}
+     <Sidebar/>
+      <Main/>
       <Footer/>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import logo1 from '../Img/GymLogo.png';
+import '../styles/Footer.css';
+const Footer = () => {
+    return(
+      <footer>
+        <div class="footer1">
+          <div class="container">
+            <ul>&copy;
+              <li>Privacy & Security</li>
+            </ul>
+          </div>
+        </div>
+        
+        <div class="footer2">
+          <div class="container">
+            <a href=""><img src={logo1} alt="Tt Logo" height = "40px" width="100px"/></a>
+            <ul>
+              <li>About</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+  export default Footer;
